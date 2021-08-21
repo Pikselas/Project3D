21ab797bab572d883ed0c95d482c69904f283f83
kashikichi <aritramaji14@outlook.com>
2021-08-21 19:57:56 +0530

    created swapchain,device and context

--- Project3D/WinMain.cpp
+++ Project3D/WinMain.cpp
@@ -1,5 +1,7 @@
 #include<Windows.h>
-
+#include<d3d11.h>
+#include<wrl.h>
+#pragma comment(lib,"d3d11.lib")
 LRESULT WINAPI customMsgPmp(HWND hndl, UINT msgcode, WPARAM wparam, LPARAM lparam)
 {
 	switch (msgcode)
@@ -25,6 +27,31 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprev, LPSTR lpcmd, int cmdsho
 	HWND handle = CreateWindowEx(0, classNm, "KS_DX_wnd", WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT,
 		800, 800, nullptr, nullptr, hinstance, nullptr);
 	ShowWindow(handle, SW_SHOW);
+
+	using Microsoft::WRL::ComPtr;
+	ComPtr<ID3D11Device> device;
+	ComPtr<ID3D11DeviceContext> context;
+	ComPtr<IDXGISwapChain> swapchain;
+
+	DXGI_SWAP_CHAIN_DESC scd = { 0 };
+	scd.BufferCount = 1;
+	scd.BufferDesc.Width = 0;
+	scd.BufferDesc.Height = 0;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferDesc.RefreshRate.Denominator = 0;
+	scd.BufferDesc.RefreshRate.Numerator = 0;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.Flags = 0u;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.OutputWindow = handle;
+	scd.SampleDesc.Count = 1;
+	scd.SampleDesc.Quality = 0;
+	scd.Windowed = true;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	
+	D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0u, nullptr, 0u, D3D11_SDK_VERSION, &scd, &swapchain,
+		&device, nullptr, &context);
 
 	MSG msg;
 	while (true)
