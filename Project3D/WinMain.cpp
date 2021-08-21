3149b94a646bbc86e26a267f419016d68ae0af02
kashikichi <aritramaji14@outlook.com>
2021-08-21 19:36:16 +0530

    registered window class

--- Project3D/WinMain.cpp
+++ Project3D/WinMain.cpp
@@ -1,5 +1,13 @@
 #include<Windows.h>
 int WinMain(HINSTANCE hinstance, HINSTANCE hprev, LPSTR lpcmd, int cmdshow)
 {
+	constexpr auto classNm = "ks_cls";
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.lpfnWndProc = DefWindowProc;
+	wc.lpszClassName = classNm;
+	wc.hInstance = hinstance;
+	wc.style = CS_OWNDC;
+	RegisterClassEx(&wc);
 	return 0;
 }
