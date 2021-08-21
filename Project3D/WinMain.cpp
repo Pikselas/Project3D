7649579cabe7c4fc5a793644b4e18f471cabc395
kashikichi <aritramaji14@outlook.com>
2021-08-21 19:40:08 +0530

    created and showed window

--- Project3D/WinMain.cpp
+++ Project3D/WinMain.cpp
@@ -9,5 +9,10 @@ int WinMain(HINSTANCE hinstance, HINSTANCE hprev, LPSTR lpcmd, int cmdshow)
 	wc.hInstance = hinstance;
 	wc.style = CS_OWNDC;
 	RegisterClassEx(&wc);
+
+	HWND handle = CreateWindowEx(0, classNm, "KS_DX_wnd", WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT,
+		800, 800, nullptr, nullptr, hinstance, nullptr);
+	ShowWindow(handle, SW_SHOW);
+
 	return 0;
 }
