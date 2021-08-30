b59aca94f96231abc77d5fbe9ae36663862331c4
kashikichi <aritramaji14@outlook.com>
2021-08-30 21:29:56 +0530

    using constant buffer

--- Project3D/VertexShader.hlsl
+++ Project3D/VertexShader.hlsl
@@ -4,10 +4,15 @@ struct vtx
 	float4 pos : SV_POSITION;
 };
 
+cbuffer cbf
+{
+	row_major matrix transform;
+};
+
 vtx main(float2 pos : POSITION, float3 col : COLOR)
 {
 	vtx Out;
-	Out.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
+	Out.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f),transform);
 	Out.col = col;
 	return Out;
 }
--- Project3D/WinMain.cpp
+++ Project3D/WinMain.cpp
@@ -115,6 +115,35 @@ int _stdcall WinMain(HINSTANCE hinstance, HINSTANCE hPrev, LPSTR lpcmd, int cmds
     graphics->CreateBuffer(&idb, &isdb, &indexBuff);
     context->IASetIndexBuffer(indexBuff.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
+    float angle = 0.1f;
+
+    struct constBuffer
+    {
+        float buff[4][4];
+    };
+
+    const constBuffer cbf =
+    {
+        {  (20/17) * std::cos(angle), (20/17) * std::sin(angle),  0.0f, 0.0f,
+           (20/17) * -std::sin(angle), (20/17) * std::cos(angle),  0.0f,   0.0f,
+             0.0f,              0.0f,           1.0f,   0.0f,
+             0.0f,              0.0f,           0.0f,   1.0f
+        }
+    };
+
+    ComPtr<ID3D11Buffer> constBuff;
+    D3D11_BUFFER_DESC cbd = { 0 };
+    cbd.ByteWidth = sizeof(cbf);
+    cbd.Usage = D3D11_USAGE_DYNAMIC;
+    cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+    cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    cbd.MiscFlags = 0u;
+    cbd.StructureByteStride = 0u;
+
+    D3D11_SUBRESOURCE_DATA cbsrd = { 0 };
+    cbsrd.pSysMem = &cbf;
+    graphics->CreateBuffer(&cbd, &cbsrd, &constBuff);
+    context->VSSetConstantBuffers(0u, 1u, constBuff.GetAddressOf());
 
     ComPtr<ID3D11VertexShader> vS;
     ComPtr<ID3DBlob> blb;
